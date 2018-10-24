d8061bd9ff3e4fda44a2f9ca4781275fd2a73c66
monaazami <32145698+monaazami@users.noreply.github.com>
2018-10-25 00:52:58 +0100

    specify the date

--- dom.js
+++ dom.js
@@ -1,12 +1,67 @@
-var body = document.querySelector("h1");
+var h = document.querySelector("h1");
 var isGray = true;
+var bodyy=document.querySelector('p');
 
-setInterval(function(){
+
+var first=setInterval(function(){
     if(isGray === true){
-        body.style.backgroundColor = "black"
+        h.style.backgroundColor = "rgb(100,255,45)"
     }else{
-        body.style.backgroundColor = "gray "
+        h.style.backgroundColor = "rgb(200,255,45) "
     }
     isGray =! isGray
-},2000);
+},500);
+
+// var second = setInterval(function(){
+//     if(isGray === true){
+//         bodyy.style.backgroundColor = "black"
+//     }else{
+//         bodyy.style.backgroundColor = "gray "
+//     }
+//     isGray =! isGray
+// },500);
+
+// var n = 0; // Counter used to verify all timers fire
+
+// function makeTimers() {
+//   var start = Date.now();
+//   for (var i = 0; i < 10000; i++, n++) {
+//     setTimeout(hello, 500);
+//   }
+//   console.log(Date.now()-start);
+//   console.log('Timers made in', Date.now() - start, 'msecs');
+// }
+
+// function hello() {
+//   if (--n == 0) {
+//     console.log('All timers fired');
+//     makeTimers(); // Do it again!
+//   }
+// }
+
+// setTimeout(makeTimers, 1000); 
+
+// console.log(new Date());
+// console.log(Date.now());
+// var clock=Math.floor(Date.now()/1000);
+// console.log(clock);
+
+
+// var timer=0;
+// var myTime = setInterval(function(){
+//     console.log(new Date());
+//     var clock=Math.floor((new Date()/1000)/60);
+//     console.log(Date.now());
+//     console.log(clock);
+// },1000);
+
+var time =new Date();
+if(time.getDay()===4){console.log("Today is Thursday!")}
+else if (time.getDay()===3){console.log("Today is Wednesday!")}
+else if (time.getDay()===2){console.log("Today is Tuesday!")}
+else if (time.getDay()===1){console.log("Today is Monday!")}
+else if (time.getDay()===0){console.log("Today is Sunday!")}
+else if (time.getDay()===5){console.log("Today is Friday!")}
+else if (time.getDay()===6){console.log("Today is Saturday!")};
+
 
